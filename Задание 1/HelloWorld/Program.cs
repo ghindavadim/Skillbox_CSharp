db02f4c520fe31a8b394535c27eb761e7b460dfc
Ghindă Vadim <112102922+ghindavadim@users.noreply.github.com>
2023-10-08 23:01:26 +0300

    Add files via upload

---
+++ Задание 1/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1 :
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
+        }
+    }
+}
